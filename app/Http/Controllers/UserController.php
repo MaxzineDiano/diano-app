1c4f9c5d49741b65670bf15858d9573083f69a51
MaxzineDiano <dianomaxzine53@gmail.com>
2025-03-01 18:16:15 +0800

    Exercise #3 - Routing, Middleware, CSRF

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,18 @@ use App\Services\UserService;
 
 class UserController extends Controller
 {
-    public function index(UserService $userService){
+    public function index(UserService $userService)
+    {
         return $userService->listUsers();
+    }
+
+    public function first(UserService $userService)
+    {
+        return collect($userService->listUsers())->first();
+    }
+
+    public function show(UserService $userService, $id)
+    {
+        return collect($userService->listUsers())->firstWhere('id', $id);
     }
 }
